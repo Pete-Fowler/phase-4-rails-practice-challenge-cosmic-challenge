9111df1ed8af95a891fa7817dafa5f14daa69a8a
Pete Fowler <fowler.pete@gmail.com>
2022-10-19 13:29:57 -0600

    Add validations to models

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,5 +1,8 @@
 class Mission < ApplicationRecord
   belongs_to :scientist
   belongs_to :planet
-  
+  validates :name, presence: true
+  validates :scientist, presence: true, uniqueness: { scope: :mission }
+  validates :planet, presence: true
+
 end
