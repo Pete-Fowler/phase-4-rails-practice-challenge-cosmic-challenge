011c577c4095ccd0f9800e951304fc59f990f0c4
Pete Fowler <fowler.pete@gmail.com>
2022-10-19 16:23:57 -0600

    Complete

--- app/controllers/scientists_controller.rb
+++ app/controllers/scientists_controller.rb
@@ -1,2 +1,38 @@
 class ScientistsController < ApplicationController
+  def index 
+    scientists = Scientist.all
+    render json: scientists
+  end
+
+  def show 
+    scientist = sci_find
+    render json: scientist, serializer: ScientistPlanetsSerializer
+  end 
+
+  def update 
+    scientist = sci_find
+    scientist.update!(scientist_params)
+    render json: scientist, status: :accepted
+  end
+
+  def create
+    scientist = Scientist.create!(scientist_params)
+    render json: scientist, status: :created
+  end
+
+  def destroy
+    scientist = sci_find
+    scientist.destroy
+    render json: {}, status: :accepted
+  end
+
+  private
+
+  def sci_find
+    Scientist.find(params[:id])
+  end
+
+  def scientist_params
+    params.permit(:name, :field_of_study, :avatar)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :planets
+  resources :planets, only: [:index]
   resources :missions, only: [:create]
   resources :scientists
 
